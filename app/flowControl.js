600877c6294b778bb73eea34be68a933cdd439a9
Arnaud Balma <delwende.arnaud@gmail.com>
2016-11-01 04:55:11 -0500

    edited flowControl.js

--- app/flowControl.js
+++ app/flowControl.js
@@ -5,12 +5,19 @@ define(function() {
     fizzBuzz : function(num) {
       // write a function that receives a number as its argument;
       // if the number is divisible by 3, the function should return 'fizz';
+      if (num%3 ==0)
+        return "fizz";
       // if the number is divisible by 5, the function should return 'buzz';
+      else if (num%5 == 0)
+        return "buzz";
       // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
+       // 'fizzbuzz';
+      else if (num%3 == 0 && num%5 == 0)
+        return "fizzbuzz"
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      else
+        
     }
   };
 });
