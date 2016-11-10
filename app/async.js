54b5c30ec2bfab46d35ebe287f286072f54a24c6
Arnaud Balma <delwende.arnaud@gmail.com>
2016-11-10 10:27:27 -0600

    async.js

--- app/async.js
+++ app/async.js
@@ -3,11 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+    	var defer = $.Deferred();
 
+      	setTimeout(function(){
+        	defer.resolve(value);
+      	}, 10);
+
+      	return defer.promise();
     },
 
     manipulateRemoteData : function(url) {
+    	var defer = $.Deferred();
 
+      	$.ajax(url).then(function(response){
+
+        var people = $.map(response.people, function(person) {
+          return person.name;
+        });
+        
+        defer.resolve(people.sort());
+      	});
+
+      return defer.promise();
     }
   };
 });
--- app/count.js
+++ app/count.js
@@ -3,9 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
- 		for (i = start; i <= end; i++){
- 			setInterval(100);
- 			console.log(i);
+ 		function counting(){
+ 			console.log(start++);
+
+ 			if(start<=end){}
+ 		}
+
+ 		function cancel(){
+ 			
+ 		}
 
  		}
     }
